58366a0dd330e8ce6fb094540387f0ef66001423
writ312 <writ0312@gmail.com>
2017-07-29 15:12:24 +0900

    update v1.0.1

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var botName = '作成したBOTの名前'
-var token = '作成したBOTのトークン'
+var botName = 'botの名前'
+var token = 'BOTのトークン'
 
 var Eris = require('eris')
 var fs = require('fs');
+var guessLanguage = require('guessLanguage').guessLanguage;
+
 var moment = date => require('moment-timezone')(date).tz('Asia/Tokyo')
 var cubeObj = {};
 
@@ -55,80 +57,57 @@ function rtnTime(addHour){
 var bot = new Eris.CommandClient(token, {}, {
     description: "Create item search url bot.",
     owner: "somebody",
-    prefix: ["!","/","@mention"]
+    prefix: ["!","/","@mention"],
+    ignoreSelf:true,
+    ignoreBots:true
 });
 
-bot.on("ready", () => { 
+bot.on("ready", (msg) => {
+
     console.log("Ready!"); 
 });
 
 bot.registerCommandAlias("halp", "help"); // Alias !halp to !help
 
-// tos j base の横断検索へのリンク
-bot.registerCommand("s", (msg, args) => { 
-    if(args.length === 0) { 
-        return "Invalid input";
-    }
-    var text = args.join(" "); 
-    return 'https://tos-jp.neet.tv/?q=' + text; 
-}, {
-    description: "Create TOS_J_BASE URL",
-    usage: "<text>"
-});
-
+// tos neet の横断検索へのリンク
 bot.registerCommand("search", (msg, args) => { 
     if(args.length === 0)
     return "Invalid input";
-    var text = args.join(" "); 
-    return 'https://tos-jp.neet.tv/?q=' + text;
+    var text = args.join(" ");
+    guessLanguage.detect(text ,function(language) {
+    text = text.replace(/\s/g,'+')
+    if(language === 'ko')
+        url =  `https://tos-kr.neet.tv/?q=${text}\nhttps://translate.google.co.jp/translate?sl=auto&tl=ja&js=y&prev=_t&hl=ja&ie=UTF-8&u=https://tos-kr.neet.tv/?q=${text}&edit-text=&act=url`; 
+    else if(language == 'ja' || language === 'zh'|| language === 'zh-TW')
+        url = 'https://tos-jp.neet.tv/?q=' + text; 
+    else
+        url = `https://tos.neet.tv/?q=${text}\nhttps://translate.google.co.jp/translate?sl=auto&tl=ja&js=y&prev=_t&hl=ja&ie=UTF-8&u=https://tos.neet.tv/?q=${text}&edit-text=&act=url`; 
+
+    bot.createMessage(msg.channel.id, url); 
+});
+
 },{ 
-    description: "Create TOS_J_BASE URL",
-    usage: "<text>"
+    description: "Create tos.neet`s url",
+    usage: "<text>",
+     aliases:["s"]
 });
 
 // 韓国語→日本語に翻訳
-bot.registerCommand("t", (msg, args) => { 
-    if(args.length === 0) { 
-        return "Invalid input";
-    }
-    var text = args.join(" ");
-    if(text.match(/http/i))
-      return `https://translate.google.co.jp/translate?sl=ko&tl=ja&js=y&prev=_t&hl=ja&ie=UTF-8&u=${text}&edit-text=&act=url`; 
-    return 'https://translate.google.co.jp/?hl=ja#ko/ja/' + text
-}, {
-    description: "translate korean to japanese",
-    usage: "<text>"
-});
-
 bot.registerCommand("trans", (msg, args) => { 
     if(args.length === 0) { 
         return "Invalid input";
     }
     var text = args.join(" ");
     if(text.match(/http/i))
-      return `https://translate.google.co.jp/translate?sl=ko&tl=ja&js=y&prev=_t&hl=ja&ie=UTF-8&u=${text}&edit-text=&act=url`; // Return the generated string
-    return 'https://translate.google.co.jp/?hl=ja#ko/ja/' + text
+      return `https://translate.google.co.jp/translate?sl=auto&tl=ja&js=y&prev=_t&hl=ja&ie=UTF-8&u=${text}&edit-text=&act=url`; 
+    return 'https://translate.google.co.jp/#auto/ja/' + text
 }, {
     description: "translate korean to japanese",
-    usage: "<text>"
+    usage: "<text>",
+    aliases:["t","translate"]
 });
 
 // 神秘的な箱
-bot.registerCommand("b", (msg, args) => {
-    if(args.length === 0) { 
-        return "Input your Character Name";
-    }
-    var text = args.join(" ");
-    var user = {channelID:msg.channel.id,mention:`<@${msg.author.id}>`,date:moment().add(24,'hour').add(-3,'minutes')}
-    bot.createMessage(user.channelID,  `${user.mention} ${moment(user.date).format('HH:mmに')}${text}さんの箱が開けられます`)
-    cubeObj[text] = user
-    setTimeout(()=>{bot.createMessage(user.channelID,  `${user.mention} ${text}さんの箱がもうすぐ開けられます!!`);cubeObj[text] = null},moment(user.date).diff(moment()))
-    fs.writeFile('MysticalCube.json', JSON.stringify(cubeObj, null, '    '));
-    },{
-        description: "translate korean to japanese",
-        usage: "<name>"
-});
-
 bot.registerCommand("box", (msg, args) => {
     if(args.length === 0) { 
         return "Input your Character Name";
@@ -141,7 +120,8 @@ bot.registerCommand("box", (msg, args) => {
     fs.writeFile('MysticalCube.json', JSON.stringify(cubeObj, null, '    '));
     },{
         description: "translate korean to japanese",
-        usage: "<name>"
+        usage: "<name>",
+        aliases:["cube","b"]
 });
 
 // 何か発言されたときに発火。ここで発言するときはif文を使って、BOTを弾かないと無限ループ
@@ -151,7 +131,8 @@ bot.on("messageCreate", (msg) => {
     //     if(mention.username === botName)
     //         bot.createMessage(msg.channel.id,'https://tos-jp.neet.tv/?q=' + msg.content.replace(/<@.*>\s*/,''))
     // }
-
+    // console.log()
+    console.log(msg.author.id)
     if(msg.author.username != botName && msg.channel.name == 'delgele'){ //ディルゲレチャンネルならタイマーを起動する
         bot.createMessage(msg.channel.id, `ディルゲレを植えました\n種まき・水やり・回収時間は ${rtnTime(0)}->${rtnTime(2)}->${rtnTime(5)}です。`);
         var obj = {date:moment(),channelID:msg.channel.id}
